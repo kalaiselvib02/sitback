ad9206217ad26fbfbbd5a6aa7a9ccfa908c3c296
kalaiselvib02 <kalaiselvi.balu@cdw.com>
2023-05-18 00:36:35 +0530

    Layout commit

---
+++ src/constants/constants.js
@@ -0,0 +1,32 @@
+export const APP_CONSTANTS = {
+    FETCH_URL: {
+      CATEGORIES : "https://jsonmockserver.vercel.app/api/shopping/furniture/categories",
+      CATEGORY_ITEM:"https://jsonmockserver.vercel.app/api/shopping/furniture/products?category="
+    },
+    CATEGORY:{
+        COUCHES:{
+            YEAR_GAURANTEE:"YEAR GAURANTEE"
+        }
+    },
+    CHECKOUT:{
+        MENU_NAV : ["MY CART" , "MY WISHLIST"],
+        TOTAL:"TOTAL AMOUNT",
+        PLACE_ORDER:"PLACE ORDER"
+    },
+    ORDER_CONFIRMATION:{
+        HEADING:"Order Confirmation",
+        CONFIRMATION_TEXT:"Thank you for shopping with us. The items will be delivered in 7 days",
+        QUANTITY:"Quantity"
+    },
+    WISH_LIST:{
+        ADD_TO_WISHLIST:"Add TO WISHLIST"
+    },
+    MY_CART : {
+        ADD_TO_CART : "ADD TO CART",
+    },
+    COPYRIGHTS:"COPYRIGHTS 2020",
+    STORAGE_KEYS:{
+     
+    },
+  };
+  
